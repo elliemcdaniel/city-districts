caf2df2fb74c760c6c0b3be7722913b47d93b84f
elliemcdaniel <ellievmcdaniel@gmail.com>
2025-05-28 18:44:44 -0700

    change basemap 3

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const map = L.map('map').setView([28.5383, -81.3792], 12); // Centered on Orlando
 
 // Add OpenStreetMap tile layer
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; OpenStreetMap & CartoDB',
-  subdomains: 'abcd'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ',
+  maxZoom: 19
 }).addTo(map);
 
 
